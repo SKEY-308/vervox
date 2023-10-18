75dd7225d1411dff6e042d816874bbb791227f74
Your Name <severus.key308@gmail.com>
2023-10-18 13:57:34 +0400

    13'57

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <div></div>
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+
+      <div className="mx-auto mb-4 flex maw-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50"></div>
+
+    </MaxWidthWrapper>
   )
 }
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,11 +1,12 @@
-import { ReactNode } from 'react'
+import { cn } from "@/lib/utils"
 
-const MaxWidthWrapper = ({ className, children }: {
-    className?: string
-    children: ReactNode
-}) => {
+const MaxWidthWrapper = ({ className, children }:
+    {
+        className?: string
+        children: React.ReactNode
+    }) => {
     return (
-        <div className='mx-auto w-full max-w-screen-xl px-2.5 md:px-20'>
+        <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
             {children}
         </div>
     )
