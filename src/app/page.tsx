eb5a7f857aed401fae262bceb8902794ba325dc6
Your Name <severus.key308@gmail.com>
2023-10-18 19:26:54 +0400

    19'26

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
 
       <div>
         <div className='relative isolate'>
+
           <div aria-hidden='true' className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
             <div
               style={{
@@ -76,8 +77,11 @@ export default function Home() {
               className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
             />
           </div>
+
         </div>
       </div>
+
+
     </>
   )
 }
