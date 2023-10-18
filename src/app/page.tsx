d6161c1d2a21349ef0e039d422eb2efa9bca884d
Your Name <severus.key308@gmail.com>
2023-10-18 16:29:20 +0400

    16'29

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,9 +7,32 @@ export default function Home() {
 
       <div className="mx-auto mb-4 flex maw-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
         <p className='text-sm font-semibold text-gray-700'>
-          Vervox is now public!
+          Vervox is now public !!
         </p>
       </div>
+
+      <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+        Chat with your{' '}
+        <span className='text-blue-600'>documents</span>{' '}
+        in seconds.
+      </h1>
+
+      <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+        Vervox allows you to have conversations with any
+        PDF document. Simply upload your file and start
+        asking questions right away.
+      </p>
+
+      {/* <Link
+        className={buttonVariants({
+          size: 'lg',
+          className: 'mt-5',
+        })}
+        href='/dashboard'
+        target='_blank'>
+        Get started{' '}
+        <ArrowRight className='ml-2 h-5 w-5' />
+      </Link> */}
 
     </MaxWidthWrapper>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -60,12 +61,12 @@ const config: Config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
